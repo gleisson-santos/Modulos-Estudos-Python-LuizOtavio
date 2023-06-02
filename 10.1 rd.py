6d3403bfec6375217a51119fdf6253f241e7ea39
Gleisson Santos <jsgleisson@gmail.com>
2023-06-02 20:39:40 -0300

    a

--- 10.1 rd.py
+++ 10.1 rd.py
@@ -13,12 +13,25 @@ inteiro = random.randrange(900, 1000, 10)
 # Gera um número de ponto flutuante entre 0 e 1
 flutuante = random.random()
 
-lista = ['Luiz', 'Otávio', 'Maria', 'Rose', 'Jenny', 'Danilo', 'Felipe']
+lista = ['Banana', 'Casa', 'Barco', 'Rosa', 'Pedr', 'Baú', 'Felipe']
 
 # Seleciona aleatóriamente valores de uma lista
-sorteio = random.sample(lista, 2)
+sorteio = random.sample(lista, 1)
 # sorteio = random.choices(lista, k=2)
 # sorteio = random.choice(lista)
+
+
+lista = ['Banana', 'Casa', 'Barco', 'Rosa', 'Pedra', 'Baú', 'Celular']
+
+# Filtra os valores a partir do terceiro item que contêm "sa" na lista
+valores_filtrados = [valor for valor in lista[2:] if "sa" in valor]
+
+# Seleciona aleatoriamente um valor da lista filtrada
+sorteio2 = random.choice(valores_filtrados)
+
+#print(sorteio2)
+
+
 
 # Embaralha a lista
 random.shuffle(lista)
---
+++ teste.py
@@ -0,0 +1,3 @@
+l = ['Banana', 'Casa', 'Barco', 'Rosa', 'Pedra', 'Baú', 'Celular']
+v = [x for x in l[2:] if 'sa' in x][:3]
+print(v)
